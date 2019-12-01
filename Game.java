8eadb866847b44922fa0d2eca4a6b55d3723027c
Krisni Almehdi <almehdi.krisni@gmail.com>
2019-12-01 17:41:03 +0100

    Fixing a problem with the character creation in Game class

--- Game.java
+++ Game.java
@@ -5,8 +5,8 @@ public class Game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> liste_choix = new ArrayList<Integer>();
-        for (int v = 1; v <= 4; v++) { liste_choix.add(v); }
+        ArrayList<String> liste_choix = new ArrayList<String>();
+        for (int v = 1; v <= 4; v++) { String adding = String.format("%d",v); liste_choix.add(adding); }
 
         System.out.println("THE WORST GAME EVER\n\n");
 
@@ -17,16 +17,16 @@ public class Game {
             String name = scanner.next();
 
             System.out.println("\nChoose your class :\n1 - Sweeper (High base Speed and Attack)\n2 - Tank (High base HP and Defense)\n3 - Mage (Ignore the enemy's defense, low base HP and DEF)\n4 - Healer (Heals an ally or inflict low damage to enemies)\n");
-            int v = scanner.nextInt();
+            String v = scanner.next();
 
             while ( liste_choix.contains(v) == false) {
                 System.out.println("\nError while choosing your class (wrong value chosen)\nChoose your class :\n1 - Sweeper (High base Speed and Attack)\n2 - Tank (High base HP and Defense)\n3 - Mage (Ignore the enemy's defense, low base HP and DEF)\n4 - Healer (Heals an ally or inflict low damage to enemies)\n");
-                v = scanner.nextInt();
+                v = scanner.next();
             }
 
-            if ( v == 1) { Sweeper c = new Sweeper(name); p1.addMember(c); }
-            else if ( v == 2 ) { Tank c = new Tank(name); p1.addMember(c); }
-            else if ( v == 3 ) { Mage c = new Mage(name); p1.addMember(c); }
+            if ( v == "1") { Sweeper c = new Sweeper(name); p1.addMember(c); }
+            else if ( v == "2" ) { Tank c = new Tank(name); p1.addMember(c); }
+            else if ( v == "3" ) { Mage c = new Mage(name); p1.addMember(c); }
             else { Healer c = new Healer(name); p1.addMember(c); }
         }
 
